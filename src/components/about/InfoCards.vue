1b13d547fa209b2b2d458b0d0035e74bd9693fb4
Nicholas <atok948@gmail.com>
2025-01-19 21:55:02 +0000

    Make changes to services page

--- src/components/about/InfoCards.vue
+++ src/components/about/InfoCards.vue
@@ -3,15 +3,15 @@
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <!-- Light Gray Card -->
         <div class="bg-gray-100 rounded-lg px-10 py-12">
-          <h2 class="text-3xl font-bold text-gray-800 mb-4">Our Mission</h2>
+          <h2 class="text-3xl font-bold text-gray-800 my-4">Our Mission</h2>
           <p class="text-gray-700 text-lg">
             To alleviate the financial burdens of investors and to establish a platform that embodies responsible and reliable business practices, transcending geographical boundaries. 
           </p>
         </div>
   
         <!-- Maroon Card -->
-        <div class="bg-[#8B1818] rounded-lg px-10 py-12">
-          <h2 class="text-3xl font-bold text-white mb-4">Our Vision</h2>
+        <div class="bg-[#8B1818] rounded-lg px-10 py-12 ">
+          <h2 class="text-3xl font-bold text-white my-4">Our Vision</h2>
           <p class="text-white text-lg">
             To set a hallmark in the precious minerals industry and become an integral part of every gold-related success story.
           </p>
@@ -19,7 +19,7 @@
   
         <!-- Yellow/Orange Card -->
         <div class="bg-[#FFA500] rounded-lg px-10 py-12">
-          <h2 class="text-3xl font-bold text-gray-800 mb-4">Our Core-Values</h2>
+          <h2 class="text-3xl font-bold text-gray-800 my-4">Our Core-Values</h2>
           <p class="text-gray-800 text-lg">
             We uphold the highest standards of integrity and ethics, and ensure profitability and sustainability. Committed to international standards and regulations.
           </p>
