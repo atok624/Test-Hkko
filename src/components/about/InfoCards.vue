0013d2839929ddfc06310d397832b6b1a9c9138e
Nicholas <atok948@gmail.com>
2025-01-19 21:42:04 +0000

    Make changes to services page

--- src/components/about/InfoCards.vue
+++ src/components/about/InfoCards.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container px-4 py-8 w-[95%] md:w-[80%] lg:w-[75%] mx-auto">
+    <div class="container px-4 py-20 w-[95%] md:w-[80%] lg:w-[75%] mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <!-- Light Gray Card -->
-        <div class="bg-gray-100 rounded-lg px-6 py-12">
+        <div class="bg-gray-100 rounded-lg px-10 py-12">
           <h2 class="text-2xl font-semibold text-gray-800 mb-4">Our mission</h2>
           <p class="text-gray-700">
             To alleviate the financial burdens of investors and to establish a platform that embodies responsible and reliable business practices, transcending geographical boundaries. 
